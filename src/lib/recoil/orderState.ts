a4611099209c02c81402a246562f0a89a6981aa6
yykim <rladuddbs789@gmail.com>
2023-10-21 14:49:58 +0900

    fix: 불필요한 파일 제거

--- src/lib/recoil/orderState.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from "recoil";
-import { CoffeeType } from "types/types";
-
-export type OrderStateType = CoffeeType & { id: number; count: number };
-
-export const orderState = atom<OrderStateType[]>({
-    key: "orderState",
-    default: [],
-});
