7f3315d9634c140ec13b4f77fb7fd165b0eb48e2
yykim <rladuddbs789@gmail.com>
2023-10-23 01:19:46 +0900

    feat: 타입 변경

--- src/models/useGetPokemonNames.ts
+++ src/models/useGetPokemonNames.ts
@@ -1,7 +1,7 @@
 // useQueres 사용해서 배열 받으면 배열 반복문 돌면서 가져온 데이터 매핑해서 디테일한 테이터로 전달하게 하자!
 import { useQueries, UseQueryOptions } from 'react-query';
 import { pokemonKey } from 'lib/queryKeyFactory';
-import { PokemonSpeciesType } from 'types/types';
+import { PokemonSpecies } from 'pokenode-ts';
 import { getApi } from 'lib/axios';
 import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
@@ -10,9 +10,10 @@ import { namesState } from 'lib/recoil/namesState';
 const useGetPokemonNames = () => {
     const [nameList, setNameList] = useRecoilState(namesState);
 
+    // 포켓몬 도감상 존재하는 포켓몬들의 수 만큼 queryFn과 key값을 만드는 함수
     const getQueries = () => {
         const result = [];
-        for (let index = 1; index < 1018; index++) {
+        for (let index = 1; index <= 1017; index++) {
             result.push({
                 queryKey: pokemonKey.pokemonName(index),
                 queryFn: () => getApi(`pokemon-species/${index}`).then((data) => data.data),
@@ -25,7 +26,7 @@ const useGetPokemonNames = () => {
 
     const [PokemonNameList] = useState(getQueries());
 
-    const speciesData = useQueries<UseQueryOptions<PokemonSpeciesType, Error>[]>(PokemonNameList);
+    const speciesData = useQueries<UseQueryOptions<PokemonSpecies, Error>[]>(PokemonNameList);
 
     useEffect(() => {
         if (speciesData[1016] && speciesData[1016].status === 'success' && nameList.length === 0) {
