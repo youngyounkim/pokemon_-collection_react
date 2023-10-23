33d5e8dd98a248ce21dab82c2b612b6796cea888
yykim <rladuddbs789@gmail.com>
2023-10-23 13:06:10 +0900

    fix: 잘못 반복되던 함수 수정

--- src/models/useGetEvolutionChain.ts
+++ src/models/useGetEvolutionChain.ts
@@ -29,15 +29,16 @@ const useGetEvolutionChain = (id: string | undefined) => {
             enabled: chainId !== 0 && !isNaN(chainId),
             select: (data) => {
                 const result: EvolutionListType = [];
+
                 const getEvolutionArr = (data: ChainLink): any => {
                     // ChainLink의 타입이 재귀형태로 반복되어 evolves_to 배열의 크기가 0일 때 까지 반복하여 진화 단계를 찾음
                     const idAndName = getIdAndName(data.species.name, data.species.url);
                     result.push(idAndName);
                     if (data.evolves_to.length === 0) {
-                        return null;
+                        return;
                     }
                     for (let index = 0; index < data.evolves_to.length; index++) {
-                        return getEvolutionArr(data.evolves_to[index]);
+                        getEvolutionArr(data.evolves_to[index]);
                     }
                 };
                 getEvolutionArr(data.chain);
