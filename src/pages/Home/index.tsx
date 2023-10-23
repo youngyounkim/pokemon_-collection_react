412eb4e1b90781170a0e9104be2fce7552d9c5e3
yykim <rladuddbs789@gmail.com>
2023-10-23 14:35:07 +0900

    feat: 무한 스크롤 적용

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import PokemonList from 'components/template/PokemonList';
 import useGetPokemonNames from 'models/useGetPokemonNames';
 import useGetPokemonList from 'models/useGetPokemonList';
 import { useNavigate } from 'react-router-dom';
+import { useCallback, useEffect, useMemo, useRef } from 'react';
 
 const Home = () => {
     const navigation = useNavigate();
@@ -12,17 +13,29 @@ const Home = () => {
         navigation(`/detail/${id}`);
     };
 
+    const targetRef = useRef<HTMLDivElement>(null);
+
+    const options = useCallback(() => {
+        return {
+            threshold: 1.0
+        };
+    }, []);
+
+    const callback = useCallback(() => {
+        fetchNextPage();
+    }, [fetchNextPage]);
+
+    const observer = useMemo(() => new IntersectionObserver(callback, options()), [callback, options]);
+
+    useEffect(() => {
+        if (targetRef.current) observer.observe(targetRef.current);
+    }, [observer]);
+
     return (
-        <div className="">
+        <main className="">
             <PokemonList pokemonItems={pokemonItems?.pages} handleDetailPage={handleDetailPage} />
-            <button
-                onClick={() => {
-                    fetchNextPage();
-                }}
-            >
-                다음 페이지
-            </button>
-        </div>
+            <div ref={targetRef} />
+        </main>
     );
 };
 
