5d1ca7d751f8ffb6f240859a4b87809f51acaab6
yykim <rladuddbs789@gmail.com>
2023-10-23 20:11:23 +0900

    feat: 모델 이름 직관적으로 변경

---
+++ src/models/useGetPokemonInfinityList.ts
@@ -0,0 +1,43 @@
+import { useInfiniteQuery } from 'react-query';
+import { pokemonKey } from 'lib/queryKeyFactory';
+import { PokemonListResponse } from 'types/types';
+import { getApi } from 'lib/axios';
+import { PokemonListStateType } from 'lib/recoil/pokemonListState';
+
+const useGetPokemonInfinityList = (pokemonList: PokemonListStateType[]) => {
+    const { data, hasPreviousPage, fetchNextPage, isFetching, isFetchingNextPage, isError } = useInfiniteQuery<
+        PokemonListResponse,
+        Error,
+        PokemonListResponse
+    >(
+        pokemonKey.pokemonListKey(),
+        ({ pageParam = 0 }) => getApi(`/pokemon/?limit=30&offset=${pageParam}`).then((data) => data.data),
+        {
+            enabled: pokemonList.length !== 0,
+            getNextPageParam: (lastPage) => {
+                const { next }: any = lastPage;
+                if (!next) return undefined;
+                return Number(new URL(next).searchParams.get('offset'));
+            },
+            select: (data) => {
+                return {
+                    ...data,
+                    pages: data.pages.map((el) => {
+                        return {
+                            ...el,
+                            results: el.results.map((el) => {
+                                const url = el.url.split('/');
+                                const nameData = pokemonList.find((item) => item.id === Number(url[6]));
+                                return { ...el, krName: nameData?.name, id: nameData?.id };
+                            })
+                        };
+                    })
+                };
+            }
+        }
+    );
+
+    return { data, hasPreviousPage, fetchNextPage, isFetching, isFetchingNextPage, isError };
+};
+
+export default useGetPokemonInfinityList;
--- src/models/useGetPokemonList.ts
+++ src/models/useGetPokemonList.ts
@@ -1,43 +1,47 @@
-import { useInfiniteQuery } from 'react-query';
+// useQueres 사용해서 배열 받으면 배열 반복문 돌면서 가져온 데이터 매핑해서 디테일한 테이터로 전달하게 하자!
+import { useQueries, UseQueryOptions } from 'react-query';
 import { pokemonKey } from 'lib/queryKeyFactory';
-import { PokemonListResponse } from 'types/types';
+import { PokemonSpecies } from 'pokenode-ts';
 import { getApi } from 'lib/axios';
-import { PokemonListStateType } from 'lib/recoil/pokemonListState';
+import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { pokemonListState } from 'lib/recoil/pokemonListState';
 
-const useGetPokemonList = (pokemonList: PokemonListStateType[]) => {
-    const { data, hasPreviousPage, fetchNextPage, isFetching, isFetchingNextPage, isError } = useInfiniteQuery<
-        PokemonListResponse,
-        Error,
-        PokemonListResponse
-    >(
-        pokemonKey.pokemonListKey(),
-        ({ pageParam = 0 }) => getApi(`/pokemon/?limit=30&offset=${pageParam}`).then((data) => data.data),
-        {
-            enabled: pokemonList.length !== 0,
-            getNextPageParam: (lastPage) => {
-                const { next }: any = lastPage;
-                if (!next) return undefined;
-                return Number(new URL(next).searchParams.get('offset'));
-            },
-            select: (data) => {
-                return {
-                    ...data,
-                    pages: data.pages.map((el) => {
-                        return {
-                            ...el,
-                            results: el.results.map((el) => {
-                                const url = el.url.split('/');
-                                const nameData = pokemonList.find((item) => item.id === Number(url[6]));
-                                return { ...el, krName: nameData?.name, id: nameData?.id };
-                            })
-                        };
-                    })
-                };
-            }
+const useGetPokemonList = () => {
+    const [pokemonList, setPokemonList] = useRecoilState(pokemonListState);
+
+    // 포켓몬 도감상 존재하는 포켓몬들의 수 만큼 queryFn과 key값을 만드는 함수
+    const getQueries = () => {
+        const result = [];
+        for (let index = 1; index <= 1017; index++) {
+            result.push({
+                queryKey: pokemonKey.pokemonName(index),
+                queryFn: () => getApi(`pokemon-species/${index}`).then((data) => data.data),
+                staleTime: Infinity,
+                suspense: true
+            });
         }
-    );
+        return result;
+    };
 
-    return { data, hasPreviousPage, fetchNextPage, isFetching, isFetchingNextPage, isError };
+    const [PokemonNameList] = useState(getQueries());
+
+    const speciesData = useQueries<UseQueryOptions<PokemonSpecies, Error>[]>(PokemonNameList);
+
+    useEffect(() => {
+        if (speciesData[1016] && speciesData[1016].status === 'success' && pokemonList.length === 0) {
+            const nameArr = speciesData.map((el) => {
+                return {
+                    id: el.data?.id,
+                    name: el.data?.names[2].name,
+                    evolutionUrl: el.data?.evolution_chain.url
+                };
+            });
+            setPokemonList(nameArr);
+        }
+    }, [speciesData, setPokemonList, pokemonList.length]);
+
+    return { pokemonList };
 };
 
 export default useGetPokemonList;
